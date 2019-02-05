5dc78d02ecfe33a0120d05d052cf94f843093504
aakashrajput <aakashsingh1999@gmail.com>
2019-02-05 13:16:50 +0530

    setup chart.js

--- assets/demo/demo.js
+++ assets/demo/demo.js
@@ -1013,7 +1013,7 @@ demo = {
 
 
 var chart_labels = ['JAN','FEB','MAR','APR','MAY','JUN','JUL','AUG','SEP','OCT','NOV','DEC'];
-    var chart_data = [ 100, 70, 90, 70, 85, 60, 75, 60, 90, 80,110, 100];
+    var chart_data = [ 180, 70, 90, 70, 85, 60, 75, 60, 90, 80,110, 100];
 
 
 var ctx = document.getElementById("chartBig1").getContext('2d');
@@ -1055,7 +1055,7 @@ var config = {
         myChartData.update();
       });
       $("#1").click(function() {
-       var chart_data = [ 80, 120, 105, 110, 95, 105, 90, 100, 80, 95,70, 120];
+       var chart_data = [ 180, 120, 105, 110, 95, 105, 90, 100, 80, 95,70, 120];
         var data = myChartData.config.data;
         data.datasets[0].data = chart_data;
         data.labels = chart_labels;
@@ -1063,7 +1063,7 @@ var config = {
       });
 
       $("#2").click(function() {
-       var chart_data = [ 60, 80, 65, 130, 80, 105, 90, 130, 70, 115,60, 130];
+       var chart_data = [ 130, 80, 65, 130, 80, 105, 90, 130, 70, 115,60, 130];
         var data = myChartData.config.data;
         data.datasets[0].data = chart_data;
         data.labels = chart_labels;
