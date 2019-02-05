586b6e85c8ec4b158f3d3133c1e2c43d08f85699
aakashrajput <aakashsingh1999@gmail.com>
2019-02-05 12:50:19 +0530

    created database connection

--- User/includes/header.php
+++ User/includes/header.php
@@ -1,4 +1,23 @@
+<?php
+session_start();
 
+  if (!isset($_SESSION['username'])) {
+    $_SESSION['msg'] = "You must log in first";
+    header('location: .././index.php');
+  }
+  if (isset($_GET['logout'])) {
+    session_destroy();
+    unset($_SESSION['username']);
+    header("location: .././index.php");
+  }
+?>
+<?php include ("db.php");
+$url = 'http://' . $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'];
+
+
+
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 
