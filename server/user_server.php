b11a3dd10b66a5607a581728aa39ebfcdf94619b
aakashrajput <aakashsingh1999@gmail.com>
2019-02-05 12:27:39 +0530

    completed login and registration

--- server/user_server.php
+++ server/user_server.php
@@ -9,7 +9,7 @@ $errors = array();
 $reg_date = date("Y/m/d");
 
 // connect to the database
-$db = mysqli_connect('localhost', 'root', '', 'Develop_Server');
+$db = mysqli_connect('localhost', 'root', '', 'Developer_Server');
 
 // REGISTER USER
 if (isset($_POST['reg_user'])) {
@@ -30,7 +30,7 @@ if (isset($_POST['reg_user'])) {
 
   // first check the database to make sure
   // a user does not already exist with the same username and/or email
-  $user_check_query = "SELECT * FROM users WHERE username='$username' OR email='$email' LIMIT 1";
+  $user_check_query = "SELECT * FROM user_reg WHERE username='$username' OR email='$email' LIMIT 1";
   $result = mysqli_query($db, $user_check_query);
   $user = mysqli_fetch_assoc($result);
 
@@ -48,12 +48,12 @@ if (isset($_POST['reg_user'])) {
   if (count($errors) == 0) {
     $password = md5($password_1);//encrypt the password before saving in the database
 
-    $query = "INSERT INTO users (reg_date, username, email, password, role)
-          VALUES('$reg_date', '$username', '$email', '$password', 'Author')";
+    $query = "INSERT INTO user_reg (reg_date, username, email, password, role, tandc)
+          VALUES('$reg_date', '$username', '$email', '$password', 'student','true')";
     mysqli_query($db, $query);
     $_SESSION['username'] = $username;
     $_SESSION['success'] = "You are now logged in";
-    header('location: ../user/');
+    header('location: ./User/');
   }
 }
 
@@ -73,43 +73,18 @@ if (isset($_POST['login_user'])) {
 
   if (count($errors) == 0) {
     $password = md5($password);
-    $query = "SELECT * FROM users WHERE username='$username' AND password='$password' AND role='Author'";
+    $query = "SELECT * FROM user_reg WHERE username='$username' AND password='$password'";
     $results = mysqli_query($db, $query);
     if (mysqli_num_rows($results) == 1) {
       $_SESSION['username'] = $username;
       $_SESSION['success'] = "You are now logged in";
-      header('location: ../user/');
+      header('location: ./User/');
     }else {
       array_push($errors, "Wrong username/password combination");
     }
   }
 }
 
-// LOGIN USER
-if (isset($_POST['login_user'])) {
-  $username = mysqli_real_escape_string($db, $_POST['username']);
-  $password = mysqli_real_escape_string($db, $_POST['password']);
-
-  if (empty($username)) {
-    array_push($errors, "Username is required");
-  }
-  if (empty($password)) {
-    array_push($errors, "Password is required");
-  }
-
-  if (count($errors) == 0) {
-    $password = md5($password);
-    $query = "SELECT * FROM users WHERE username='$username' AND password='$password' AND role = 'Admin'";
-    $results = mysqli_query($db, $query);
-    if (mysqli_num_rows($results) == 1) {
-      $_SESSION['username'] = $username;
-      $_SESSION['success'] = "You are now logged in";
-      header('location: ../admin/');
-    }else {
-      array_push($errors, "Wrong username/password combination");
-    }
-  }
-}
 
 
 ?>
