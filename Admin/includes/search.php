0f269885c418fe6f76df7e15b9d3ba1acafa407f
aakashrajput <aakashsingh1999@gmail.com>
2019-02-05 12:47:08 +0530

    created routes

---
+++ Admin/includes/search.php
@@ -0,0 +1,5 @@
+<li class="search-bar input-group">
+  <button class="btn btn-link" id="search-button" data-toggle="modal" data-target="#searchModal"><i class="tim-icons icon-zoom-split"></i>
+    <span class="d-lg-none d-md-block">Search</span>
+  </button>
+</li>
