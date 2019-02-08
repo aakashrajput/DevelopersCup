229696d4d58faaa0bd85e4984a6369ad4cbf766d
aakashrajput <aakashsingh1999@gmail.com>
2019-02-08 23:03:13 +0530

    Updated Menus

--- Admin/includes/header.php
+++ Admin/includes/header.php
@@ -53,14 +53,16 @@ $url = 'http://' . $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'];
         Tip 1: You can change the color of the sidebar using: data-color="blue | green | orange | red"
     -->
       <div class="sidebar-wrapper">
-        <div class="logo">
-          <a href="javascript:void(0)" class="simple-text logo-mini">
-            CT
-          </a>
-          <a href="javascript:void(0)" class="simple-text logo-normal">
-            Creative Tim
-          </a>
-        </div>
+          <div class="logo">
+            <a href="javascript:void(0)" class="simple-text logo-mini">
+              <img src="../assets/img/mike.jpg" alt="Profile Photo">
+            </a>
+            <a href="javascript:void(0)" class="simple-text logo-normal">
+                <?php  if (isset($_SESSION['admin_username'])) : ?>
+                            <?php echo $_SESSION['admin_username']; ?>
+               <?php endif ?>
+            </a>
+          </div>
         <ul class="nav">
           <li class="active">
             <a href="./">
@@ -69,49 +71,113 @@ $url = 'http://' . $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'];
             </a>
           </li>
           <li>
-            <a data-toggle="collapse" href="#pagesExamples">
-              <i class="tim-icons icon-image-02"></i>
+            <a href="user_list.php">
+              <i class="tim-icons icon-paper"></i>
+              <p>User List</p>
+            </a>
+          </li>
+
+          <li>
+            <a data-toggle="collapse" href="#ContestPage">
+              <i class="tim-icons icon-support-17"></i>
               <p>
-                Pages
+                Mentor
+                <b class="caret"></b>
+              </p>
+            </a>
+            <div class="collapse" id="ContestPage">
+              <ul class="nav">
+                <li>
+                  <a href="../examples/pages/pricing.html">
+                    <span class="sidebar-mini-icon">+</span>
+                    <span class="sidebar-normal"> Add </span>
+                  </a>
+                </li>
+                <li>
+                  <a href="../examples/pages/rtl.html">
+                    <span class="sidebar-mini-icon">-</span>
+                    <span class="sidebar-normal">Remove</span>
+                  </a>
+                </li>
+                <li>
+                  <a href="./mentor_list.php">
+                    <span class="sidebar-mini-icon">L</span>
+                    <span class="sidebar-normal">List</span>
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </li>
+          <li>
+            <a data-toggle="collapse" href="#ContestPage">
+              <i class="tim-icons icon-planet"></i>
+              <p>
+                Contest
+                <b class="caret"></b>
+              </p>
+            </a>
+            <div class="collapse" id="ContestPage">
+              <ul class="nav">
+                <li>
+                  <a href="../examples/pages/pricing.html">
+                    <span class="sidebar-mini-icon">C.C</span>
+                    <span class="sidebar-normal"> Create Contest </span>
+                  </a>
+                </li>
+                <li>
+                  <a href="../examples/pages/rtl.html">
+                    <span class="sidebar-mini-icon">C.L</span>
+                    <span class="sidebar-normal">Contest List</span>
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </li>
+          <li>
+            <a data-toggle="collapse" href="#pagesExamples">
+              <i class="tim-icons icon-volume-98"></i>
+              <p>
+                Events
                 <b class="caret"></b>
               </p>
             </a>
             <div class="collapse" id="pagesExamples">
               <ul class="nav">
                 <li>
-                  <a href="./pages/pricing.php">
-                    <span class="sidebar-mini-icon">P</span>
-                    <span class="sidebar-normal"> Pricing </span>
+                  <a href="../examples/pages/pricing.html">
+                    <span class="sidebar-mini-icon">C.E</span>
+                    <span class="sidebar-normal"> Create Event </span>
                   </a>
                 </li>
                 <li>
-                  <a href="./pages/timeline.php">
-                    <span class="sidebar-mini-icon">T</span>
-                    <span class="sidebar-normal"> Timeline </span>
+                  <a href="../examples/pages/rtl.html">
+                    <span class="sidebar-mini-icon">E.L</span>
+                    <span class="sidebar-normal">Event List</span>
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </li>
+          <li>
+            <a data-toggle="collapse" href="#pagesExamples2">
+              <i class="tim-icons icon-image-02"></i>
+              <p>
+                Tasks
+                <b class="caret"></b>
+              </p>
+            </a>
+            <div class="collapse" id="pagesExamples2">
+              <ul class="nav">
+                <li>
+                  <a href="../examples/pages/pricing.html">
+                    <span class="sidebar-mini-icon">C.T</span>
+                    <span class="sidebar-normal"> Create Tasks By Event </span>
                   </a>
                 </li>
                 <li>
-                  <a href="./pages/login.php">
-                    <span class="sidebar-mini-icon">L</span>
-                    <span class="sidebar-normal"> Login </span>
-                  </a>
-                </li>
-                <li>
-                  <a href="./pages/register.php">
-                    <span class="sidebar-mini-icon">R</span>
-                    <span class="sidebar-normal"> Register </span>
-                  </a>
-                </li>
-                <li>
-                  <a href="./pages/lock.php">
-                    <span class="sidebar-mini-icon">LS</span>
-                    <span class="sidebar-normal"> Lock Screen </span>
-                  </a>
-                </li>
-                <li>
-                  <a href="./pages/user.php">
-                    <span class="sidebar-mini-icon">UP</span>
-                    <span class="sidebar-normal"> User Profile </span>
+                  <a href="../examples/pages/rtl.html">
+                    <span class="sidebar-mini-icon">T.L</span>
+                    <span class="sidebar-normal">Taks List</span>
                   </a>
                 </li>
               </ul>
